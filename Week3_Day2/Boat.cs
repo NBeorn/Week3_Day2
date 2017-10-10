67079cf23782ca7dd423b82f746c159849d43ea8
NBeorn <nmbeorn@gmail.com>
2017-10-10 11:56:55 -0400

    done with OOP

--- Week3_Day2/Boat.cs
+++ Week3_Day2/Boat.cs
@@ -9,31 +9,46 @@ namespace Week3_Day2
     class Boat
     {
         public int numberOfPassengers;
-
         private string name;
         private double engineSize;
 
         public string Name
         {
-            get { return name; }
-            set { name = value; }
+            get { return this.name; }
+            set { this.name = value; }
         }
-
 
         public Boat()
         {
 
         }
 
+        public Boat(int numberOfPassengers, string name, double engineSize)
+        {
+            this.numberOfPassengers = numberOfPassengers;
+            this.name = name;
+            this.engineSize = engineSize;
+        }
+
         public void Move()
         {
             //code to make the boat move
+            Console.WriteLine(name + " moves forward");
         }
 
         private double CalculateMPG()
         {
-            //code to calculate the MPG
+            double MPG = engineSize * .7d;
             return MPG;
+        }
+
+        public void BoatInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Passengers: " + numberOfPassengers);
+            Console.WriteLine("Engine size: " + engineSize);
+            Console.WriteLine("MPG: " + CalculateMPG());
+            Console.WriteLine();
         }
     }
 }
