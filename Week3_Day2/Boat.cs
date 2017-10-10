a76b5286f4d1b256263404ac5dc1518d68fc2cf1
NBeorn <nmbeorn@gmail.com>
2017-10-10 10:01:39 -0400

    Initial commit

---
+++ Week3_Day2/Boat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Day2
+{
+    class Boat
+    {
+        public int numberOfPassengers;
+
+        private string name;
+        private double engineSize;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        public Boat()
+        {
+
+        }
+
+        public void Move()
+        {
+            //code to make the boat move
+        }
+
+        private double CalculateMPG()
+        {
+            //code to calculate the MPG
+            return MPG;
+        }
+    }
+}
